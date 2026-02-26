1bfc39da021b4a2293ccf884e9880eec997a39e9
Joseph <josephelforzli@gmail.com>
2026-02-26 16:58:14 +0100

    test

--- init_EC2.sh
+++ init_EC2.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+GIT_HUB_EC2_connect=https://github.com/joseph-elf/EC2_connect.git
+
+
+REMOTE_COMMANDS="
+echo 'HEY'
+git clone $GIT_HUB_EC2_connect;
+ls -a;
+grep -qxF 'export PATH=\"\$HOME/EC2_connect:\$PATH\"' ~/.bashrc || \
+echo 'export PATH=\"\$HOME/EC2_connect:\$PATH\"' >> ~/.bashrc
+source ~/.bashrc;
+"
+
+
 if [[ ! -f ./config_EC2.sh ]]; then
     echo "Error: config_EC2.sh not found in the current directory!"
     exit 1
@@ -12,4 +25,4 @@ echo "Trying to connect to "$IP
 echo "as "$USERNAME
 echo "with the key located in "$SSH_FILE
 
-ssh -i $SSH_FILE -t $USERNAME@$IP
+ssh -i $SSH_FILE -o StrictHostKeyChecking=no -t $USERNAME@$IP "$REMOTE_COMMANDS exec bash"
