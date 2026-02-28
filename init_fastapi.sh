3e1dbf89f1035150b7c4717e20f6263395d80b4a
Joseph <josephelforzli@gmail.com>
2026-02-28 01:19:25 +0100

    update fastapi

--- init_fastapi.sh
+++ init_fastapi.sh
@@ -9,7 +9,7 @@ sudo apt install nginx -y
 
 
 
-cat << EOF > /etc/nginx/sites-available/fastapi
+sudo tee /etc/nginx/sites-available/fastapi > /dev/null << EOF
 server {
     listen 80;
     server_name _;
@@ -23,11 +23,33 @@ server {
 EOF
 
 
-sudo ln -s /etc/nginx/sites-available/fastapi /etc/nginx/sites-enabled/
+sudo ln -sf /etc/nginx/sites-available/fastapi /etc/nginx/sites-enabled/
 
-
-sudo rm /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo nginx -t
 
 sudo systemctl restart nginx
+
+
+sudo tee /etc/systemd/system/fastapi.service > /dev/null << EOF
+[Unit]
+Description=FastAPI App
+After=network.target
+
+[Service]
+User=ubuntu
+WorkingDirectory=/home/ubuntu/API_rene
+ExecStart=/home/ubuntu/venv/bin/gunicorn main:app -c /home/ubuntu/API_rene/config_gunicorn.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+sudo systemctl daemon-reload
+
+sudo systemctl start fastapi
+
+sudo systemctl enable fastapi
